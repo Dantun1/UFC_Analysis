83e38ca8b429e66db1a4537222f7d70ab62c5790
Dantun1 <daniel@tunyan.com>
2025-07-20 17:21:05 +0200

    All base templates set up. Simple Fighter overview views.

--- ufc_webapp/fighter_analysis/urls.py
+++ ufc_webapp/fighter_analysis/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import TestView
-
+from .views import HomeView, FightersView, SearchFighterView, FighterOverviewView, BlogView, EventsView, AboutView, \
+    ModelsView
 
 urlpatterns = [
-    path("", TestView.as_view(), name="test"),
+    path("", HomeView.as_view(), name="home"),
+    path("fighters", FightersView.as_view(), name="fighters"),
+    path("fighters/<int:pk>", FighterOverviewView.as_view(), name="fighter_overview"),
+    path("search", SearchFighterView.as_view(), name="search"),
+    path("blog", BlogView.as_view(), name="blog"),
+    path("events", EventsView.as_view(), name="events"),
+    path("about", AboutView.as_view(), name="about"),
+    path("models", ModelsView.as_view(), name="models")
 ]
